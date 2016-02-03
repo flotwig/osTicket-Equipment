a10cddcc21457c3216ad39fc62ff3522d04e9ac0
Zach Bloomquist <zrbq@live.com>
2016-02-03 13:15:59 -0500

    templating

--- controller/controller.php
+++ controller/controller.php
@@ -1,13 +1,14 @@
 <?php
 namespace controller;
 class Controller{
-    private function render(){
+    private function render($template,$variables){
         $loader = new Twig_Loader_Filesystem(EQUIPMENT_PATH.'templates');
         $twig = new Twig_Environment($loader,array(
            'cache' => EQUIPMENT_PATH.'cache',
         ));
+        $template = $twig->loadTemplate($template);
         require_once(STAFFINC_DIR.'header.inc.php');
-
+        echo $template->render($variables);
         require_once(STAFFINC_DIR.'footer.inc.php');
     }
 }
